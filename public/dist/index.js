e6376e17e0cc01e7aacc115e2e714059a43c36b8
Leticia Leal <leticialealp@hotmail.com>
2022-06-03 09:44:43 -0300

    adição da funcionalidade de cadastrar usuario

--- public/dist/index.js
+++ public/dist/index.js
@@ -8,3 +8,59 @@ btnAcessar.addEventListener('click', () => {
 btnCadastrar.addEventListener('click', () => {
     container.classList.add('painel-direito-ativo');
 });
+// CADASTRO DE UM USUARIO
+let formularioCadastro = document.querySelector('#formulario-cadastro');
+let inputCadastroNome = document.querySelector('#input-cadastro-nome');
+let inputCadastroEmail = document.querySelector('#input-cadastro-email');
+let inputCadastroSenha = document.querySelector('#input-cadastro-senha');
+formularioCadastro.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    verificaCampos();
+});
+function verificaCampos() {
+    if (inputCadastroNome.value === '' || inputCadastroNome.value.length < 3) {
+        inputCadastroNome.focus();
+        inputCadastroNome.setAttribute('style', 'outline-color: red');
+        return;
+    }
+    if (inputCadastroEmail.value === '' || inputCadastroEmail.value.length < 10) {
+        inputCadastroEmail.focus();
+        inputCadastroEmail.setAttribute('style', 'outline-color: red');
+        return;
+    }
+    if (inputCadastroSenha.value === '' || inputCadastroSenha.value.length < 8) {
+        inputCadastroSenha.focus();
+        inputCadastroSenha.setAttribute('style', 'outline-color: red');
+        return;
+    }
+    inputCadastroNome.removeAttribute('style');
+    inputCadastroEmail.removeAttribute('style');
+    inputCadastroSenha.removeAttribute('style');
+    let novoUsuario = {
+        nome: inputCadastroNome.value,
+        login: inputCadastroEmail.value,
+        senha: inputCadastroSenha.value,
+        recados: []
+    };
+    cadastrarUsuario(novoUsuario);
+}
+function cadastrarUsuario(novoUsuario) {
+    let listaUsuarios = JSON.parse(localStorage.getItem('usuarios') || '[]');
+    let existe = listaUsuarios.some((usuario) => {
+        return usuario.login === novoUsuario.login;
+    });
+    if (existe) {
+        let confirma = confirm("Este e-mail já está cadastrado. Deseja ir para a página de login?");
+        if (confirma) {
+            container.classList.remove('painel-direito-ativo');
+        }
+        return;
+    }
+    listaUsuarios.push(novoUsuario);
+    localStorage.setItem('usuarios', JSON.stringify(listaUsuarios));
+    alert('Conta criada com sucesso!');
+    formularioCadastro.reset();
+    setTimeout(() => {
+        container.classList.remove('painel-direito-ativo');
+    }, 1000);
+}
--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,92 @@ btnCadastrar.addEventListener('click', () => {
     container.classList.add('painel-direito-ativo');
 });
 
+
+// CADASTRO DE UM USUARIO
+let formularioCadastro = document.querySelector('#formulario-cadastro') as HTMLFormElement;
+let inputCadastroNome = document.querySelector('#input-cadastro-nome') as HTMLInputElement;
+let inputCadastroEmail = document.querySelector('#input-cadastro-email') as HTMLInputElement;
+let inputCadastroSenha = document.querySelector('#input-cadastro-senha') as HTMLInputElement;
+
+interface Usuario {
+    nome: string,
+    login: string,
+    senha: string,
+    recados: any[]
+}
+
+formularioCadastro.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+
+    verificaCampos();
+});
+
+function verificaCampos(): void {
+
+    if (inputCadastroNome.value === '' || inputCadastroNome.value.length < 3) {
+        inputCadastroNome.focus();
+        inputCadastroNome.setAttribute('style', 'outline-color: red');
+        return
+    }
+
+    if (inputCadastroEmail.value === '' || inputCadastroEmail.value.length < 10) {
+        inputCadastroEmail.focus();
+        inputCadastroEmail.setAttribute('style', 'outline-color: red');
+        return
+    }
+
+    if (inputCadastroSenha.value === '' || inputCadastroSenha.value.length < 8) {
+        inputCadastroSenha.focus();
+        inputCadastroSenha.setAttribute('style', 'outline-color: red');
+        return
+    }
+
+    inputCadastroNome.removeAttribute('style');
+    inputCadastroEmail.removeAttribute('style');
+    inputCadastroSenha.removeAttribute('style');
+
+    let novoUsuario: Usuario = {
+        nome: inputCadastroNome.value,
+        login: inputCadastroEmail.value,
+        senha: inputCadastroSenha.value,
+        recados: []
+    }
+
+    cadastrarUsuario(novoUsuario);
+}
+
+function cadastrarUsuario(novoUsuario: Usuario) {
+    let listaUsuarios: Array<Usuario> = buscarUsuariosNoStorage();
+
+    let existe: boolean = listaUsuarios.some((usuario) => {
+        return usuario.login === novoUsuario.login
+    });
+
+    if (existe) {
+        let confirma = confirm("Este e-mail já está cadastrado. Deseja ir para a página de login?");
+
+        if (confirma) {
+            container.classList.remove('painel-direito-ativo');
+            formularioCadastro.reset();
+        }
+        return
+    }
+
+    listaUsuarios.push(novoUsuario);
+    localStorage.setItem('usuarios', JSON.stringify(listaUsuarios));
+
+    alert('Conta criada com sucesso!');
+    formularioCadastro.reset();
+
+    setTimeout(() => {
+        container.classList.remove('painel-direito-ativo');
+    }, 1000);
+
+}
+
+function buscarUsuariosNoStorage() {
+
+    return JSON.parse(localStorage.getItem('usuarios') || '[]');
+}
+
+// LOGAR O USUARIO NA APLICAÇÃO 
